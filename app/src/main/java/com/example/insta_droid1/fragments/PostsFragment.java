46ef443e2f32a29180ef24f8a47d0f3a28a79469
claudia00 <claudia.martinez91@qmail.cuny.edu>
2019-03-26 10:34:59 -0400

    created class: PostsAdapter, implemented Glide library, created item_post.xml, modified query posts to show images and descriptions

--- app/src/main/java/com/example/insta_droid1/fragments/PostsFragment.java
+++ app/src/main/java/com/example/insta_droid1/fragments/PostsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -11,11 +12,13 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.insta_droid1.Post;
+import com.example.insta_droid1.PostsAdapter;
 import com.example.insta_droid1.R;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PostsFragment extends Fragment {
@@ -23,6 +26,8 @@ public class PostsFragment extends Fragment {
     public static final String TAG = "PostsFragment";
 
     private RecyclerView rvPosts;
+    private PostsAdapter adapter;
+    private List<Post> mPosts;
 
 //onCreateView inflate in the view
     @Nullable
@@ -36,10 +41,15 @@ public class PostsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         rvPosts = view.findViewById(R.id.rvPosts);
 
-        //create the adapter
         //create the data source
+        mPosts = new ArrayList<>();
+        //create the adapter
+        adapter = new PostsAdapter(getContext(), mPosts);
         //set the adapter on the recycler view
+        rvPosts.setAdapter(adapter);
         //set the layout manager on the recycler view
+        rvPosts.setLayoutManager(new LinearLayoutManager(getContext()));
+
         queryPosts();
 
         //super.onViewCreated(view, savedInstanceState);
@@ -49,24 +59,24 @@ public class PostsFragment extends Fragment {
         ParseQuery<Post> postQuery = new ParseQuery<Post>(Post.class);
         //to include more details on description
         postQuery.include(Post.KEY_USER);
-
         //getting the info from database
         postQuery.findInBackground(new FindCallback<Post>() {
             @Override
             public void done(List<Post> posts, ParseException e) {
-                if(e!= null){
+                if (e != null) {
                     Log.e(TAG, "Error with query");
                     e.printStackTrace();
                     return;
                 }
+                mPosts.addAll(posts);
+                adapter.notifyDataSetChanged();
 
-                for(int i = 0; i< posts.size(); i++){
+                for (int i = 0; i < posts.size(); i++) {
                     Post post = posts.get(i);
-                    Log.d(TAG, "Post: " + post.getDescription()+ ", Username: " + post.getUser().getUsername());
+                    Log.d(TAG, "Post: " + post.getDescription() + ", Username: " + post.getUser().getUsername());
                 }
             }
         });
-
-    }
+        }
 
 }
